3b3465c52160dd390523f25ff130c0b2a552d24a
fa18swiss <fa18swiss@users.noreply.github.com>
2019-12-17 00:17:16 +0100

    download all possible dates items

--- app/VfrManual.py
+++ app/VfrManual.py
@@ -9,7 +9,7 @@ from .DataFile import DataFile
 class VfrManual:
 
     __URLs = {
-        "en": "https://www.skybriefing.com/portal/evfr-manual-gen",
+        "en": "https://www.skybriefing.com/portal/en/evfr-manual-gen",
         "fr": "https://www.skybriefing.com/portal/fr/evfr-manual-gen",
         "de": "https://www.skybriefing.com/portal/de/evfr-manual-gen",
         "it": "https://www.skybriefing.com/portal/it/evfr-manual-gen"
@@ -27,39 +27,33 @@ class VfrManual:
             self.__logger.debug("No update need")
             return
         try:
-            parseds = {}
+            parseds = []
             for lang, url in self.__URLs.items():
                 response = requests.get(url)
                 parsed_body = html.fromstring(response.text)
                 xpath_date = '//*[@id="column-1"]/table/tbody/tr/td[1]/strong/span'
                 xpath_link = '//*[@id="column-1"]/table/tbody/tr/td[4]//a/@href'
 
-                date = parsed_body.xpath(xpath_date)
-                link = parsed_body.xpath(xpath_link)
+                dates = parsed_body.xpath(xpath_date)
+                links = parsed_body.xpath(xpath_link)
 
-                self.__logger.debug("Raw date=%s link=%s", date, link)
+                self.__logger.debug("Raw dates=%s links=%s", dates, links)
 
-                date = date[0].text_content().strip()
-                date = " ".join(date.split(" ")[-3:])
-                date = datetime.datetime.strptime(date, "%d %b %Y")
-                date = date.date()
-                date = date.isoformat()
-                link = urljoin(url, link[0])
+                if len(dates) != len(links):
+                    raise Exception("Invalid len date:%d link:%d" % (len(dates), len(links)))
 
-                self.__logger.debug("Decoded date=%s link=%s", date, link)
-                parseds[lang] = (date, link)
+                for i in range(len(dates) - 1, -1, -1):
+                    date = dates[i].text_content().strip()
+                    date = " ".join(date.split(" ")[-3:])
+                    date = datetime.datetime.strptime(date, "%d %b %Y")
+                    date = date.date()
+                    date = date.isoformat()
+                    link = urljoin(url, links[i])
 
-            common_date = None
-            for lang, d in parseds.items():
-                date, link = d
-                if common_date is None:
-                    common_date = date
-                elif date != common_date:
-                    raise Exception("Date mismatch %s %s"% (date, common_date))
+                    self.__logger.debug("Decoded date=%s link=%s", date, link)
+                    parseds.append((lang, date, link))
 
-            for lang, d in parseds.items():
-                date, link = d
-
+            for lang, date, link in parseds:
                 if self.data_file.contains(lang, date):
                     self.__logger.info("Date %s ok for %s", date, lang)
                     self.data_file.touch()

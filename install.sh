d39877cd5140bb11493aca299d0323912f674539
fa18swiss <fa18swiss@users.noreply.github.com>
2019-12-16 23:16:47 +0100

    Launch gulp

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+npm install gulp-cli -g
+
+yarn install
+
+gulp
--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-docker run -it --rm -w /app -v `pwd`:/app node yarn install
+docker run \
+	-it \
+	--rm \
+	-u $(id -u):$(id -g) \
+	-w /app \
+	-v `pwd`:/app \
+	node \
+	./install.sh
 
 
 app=vfrmanualapi
@@ -9,4 +16,10 @@ ver=1.0
 docker build -t $app:$ver .
 docker stop $app
 docker rm $app
-docker run -d -p 127.0.0.1:8003:80 --name=$app --restart=always -v `pwd`:/app $app:$ver
+docker run \
+	-d \
+	-p 127.0.0.1:8003:80 \
+	--name=$app \
+	--restart=always \
+	-v `pwd`:/app \
+	$app:$ver

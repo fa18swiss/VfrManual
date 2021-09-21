7e55b303577581fd8f94074989953a8278cad00f
fa18swiss <fa18swiss@users.noreply.github.com>
2021-09-21 22:47:59 +0200

    remove sourcemap in destination

---
+++ gulp-remove-sourcemap.js
@@ -0,0 +1,35 @@
+'use strict';
+// Source : https://github.com/ferares/gulp-remove-sourcemaps
+var Transform = require('stream').Transform;
+
+const PLUGIN_NAME = 'gulp-remove-sourcemaps';
+
+module.exports = function() {
+    var transformStream = new Transform({objectMode: true});
+    /**
+     * @param {Buffer|string} file
+     * @param {string} encoding - ignored if file contains a Buffer
+     * @param {function(Error, object)} callback - Call this function (optionally with an
+     *          error argument and data) when you are done processing the supplied chunk.
+     */
+    transformStream._transform = function(file, encoding, callback) {
+        if (file.isNull()) {
+            return callback(null, file);
+        }
+
+        if (file.isStream()) {
+            return callback(PLUGIN_NAME + ": Streams not supported!", undefined);
+        }
+
+        let contents = file.contents.toString(encoding);
+
+        contents = contents.replace(/\/\*# sourceMappingURL=.*\\*/g, '')
+        contents = contents.replace(/\/\/# sourceMappingURL=.*\\*/g, '')
+
+        file.contents = Buffer.from(contents, encoding);
+
+        callback(null, file);
+    };
+
+    return transformStream;
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,34 @@
 const { src, dest, series, parallel } = require("gulp");
 const del = require("del");
 const rename = require("gulp-rename");
+const removeSourcemaps = require("./gulp-remove-sourcemap");
 
 const dir_css = "app/static/css/";
 const dir_js = "app/static/js/";
 const dir_svg = "app/static/svg/";
 
 function css() {
-    return src("node_modules/bootstrap/dist/css/bootstrap.min.css").pipe(dest(dir_css));
+    return src("node_modules/bootstrap/dist/css/bootstrap.min.css")
+        .pipe(removeSourcemaps())
+        .pipe(dest(dir_css));
 }
 
 function js_bootstrap() {
-    return src("node_modules/bootstrap/dist/js/bootstrap.min.js").pipe(dest(dir_js));
+    return src("node_modules/bootstrap/dist/js/bootstrap.min.js")
+        .pipe(removeSourcemaps())
+        .pipe(dest(dir_js));
 }
 
 function js_jquery() {
-    return src("node_modules/jquery/dist/jquery.slim.min.js").pipe(dest(dir_js));
+    return src("node_modules/jquery/dist/jquery.slim.min.js")
+        .pipe(removeSourcemaps())
+        .pipe(dest(dir_js));
 }
 
 function js_popper() {
-    return src("node_modules/popper.js/dist/umd/popper.min.js").pipe(dest(dir_js));
+    return src("node_modules/popper.js/dist/umd/popper.min.js")
+        .pipe(removeSourcemaps())
+        .pipe(dest(dir_js));
 }
 
 function svg_flags_de() {

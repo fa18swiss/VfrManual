8defacaf790a7afda3cf5087811e0455b682f7e4
fa18swiss <fa18swiss@users.noreply.github.com>
2022-04-04 22:36:49 +0200

    Cache all resources (automatically)

--- app/sw.js
+++ app/sw.js
@@ -3,14 +3,28 @@ const cacheName = `vfrmanual-${version}`;
 
 const CACHE_URLS = [
     '/',
+    '/static/app.js',
     '/static/css/bootstrap.min.css',
-    '/static/js/jquery.slim.min.js',
+    '/static/icon/android-chrome-192x192.png',
+    '/static/icon/android-chrome-512x512.png',
+    '/static/icon/apple-touch-icon.png',
+    '/static/icon/browserconfig.xml',
+    '/static/icon/favicon-16x16.png',
+    '/static/icon/favicon-32x32.png',
+    '/static/icon/favicon.ico',
+    '/static/icon/mstile-144x144.png',
+    '/static/icon/mstile-150x150.png',
+    '/static/icon/mstile-310x150.png',
+    '/static/icon/mstile-310x310.png',
+    '/static/icon/mstile-70x70.png',
+    '/static/icon/safari-pinned-tab.svg',
+    '/static/icon/site.webmanifest',
     '/static/js/bootstrap.min.js',
+    '/static/js/jquery.slim.min.js',
     '/static/svg/de.svg',
     '/static/svg/en.svg',
     '/static/svg/fr.svg',
-    '/static/svg/it.svg',
-    '/static/app.js'
+    '/static/svg/it.svg'
 ];
 
 self.addEventListener('install', e => {
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const { src, dest, series, parallel } = require("gulp");
 const del = require("del");
 const rename = require("gulp-rename");
 const removeSourcemaps = require("./gulp-remove-sourcemap");
+const modifyFile = require('gulp-modify-file')
+const fs = require("fs");
 
 const dir_css = "app/static/css/";
 const dir_js = "app/static/js/";
@@ -46,10 +48,44 @@ function clean() {
     ]);
 }
 
+function readFiles(path, res) {
+    if (!res) res = [];
+    let tab = fs.readdirSync(path, {withFileTypes :true});
+    for(let i = 0 ; i < tab.length ; i++) {
+        let it = tab[i];
+        let item = `${path}/${it.name}`
+        if (it.isDirectory()){
+            readFiles(item, res)
+        } else {
+            res.push(item)
+        }
+    };
+    return res
+}
+
+function swPath() {
+    return src("app/sw.js")
+        .pipe(modifyFile(content => {
+            let files = readFiles("app/static");
+            files.push("app/")
+            files.sort();
+            let p1 = content.indexOf("CACHE_URLS = [") + 14;
+            let crlf = content.substring(p1, p1 + 2);
+            if (crlf[1] === " ") crlf = crlf.substring(0, 1);
+            let p2 = content.indexOf("]", p1);
+            let res = content.substring(0, p1);
+            for (let i = 0; i < files.length; i++) {
+                res = `${res}${i === 0 ? "" : ","}${crlf}    '${files[i].substring(3)}'`;
+            }
+            return `${res}${crlf}${content.substring(p2)}`;
+        }))
+        .pipe(dest('app'))
+}
+
 const svg_flags = parallel(svg_flags_de, svg_flags_en, svg_flags_fr, svg_flags_it);
 const svg = parallel(svg_flags);
 const js = parallel(js_bootstrap, js_jquery);
-const build = parallel(css, js, svg);
+const build = series(parallel(css, js, svg), swPath);
 
 exports.default = series(clean, build);
 exports.build = build;

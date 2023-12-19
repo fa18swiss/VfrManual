ffdb64b3c542c9500333293a5e0c20b372da6377
fa18swiss <fa18swiss@users.noreply.github.com>
2023-12-19 19:00:36 +0100

    Load version from package.json and update it in start.sh and sw.js

--- Publish.ps1
+++ Publish.ps1
@@ -3,10 +3,24 @@ gulp clean
 gulp build
 
 $app = "vfrmanualapi"
-$version = Get-content .\app\sw.js `
-    | Select-String "version = ""([0-9\.]+)""" `
+$version = Get-content .\package.json `
+    | Select-String """version"": ""([0-9\.]+)""" `
     | ForEach-Object { $_.Matches } `
     | ForEach-Object { $_.Groups[1].Value }
+
+Write-Host "Version : $version"
+
+$root = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($MyInvocation.MyCommand.Path, ".."))
+
+$start = [System.IO.Path]::Combine($root, "start.sh")
+$txt = [System.IO.File]::ReadAllText($start)
+$txt = [System.Text.RegularExpressions.Regex]::Replace($txt, "ver=([0-9\.]+)", "ver=$version")
+[System.IO.File]::WriteAllText($start, $txt)
+
+$sw = [System.IO.Path]::Combine($root, "app", "sw.js")
+$txt = [System.IO.File]::ReadAllText($sw)
+$txt = [System.Text.RegularExpressions.Regex]::Replace($txt, "version = ""([0-9\.]+)""", "version = ""$version""")
+[System.IO.File]::WriteAllText($sw, $txt)
 
 $id = "${app}:${version}"
 $tar = "${app}_${version}.tar"

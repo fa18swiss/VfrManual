5f374f8e620e30a1afe2716f9d267c3e49e81fc9
fa18swiss <fa18swiss@users.noreply.github.com>
2023-12-19 19:11:42 +0100

    Version 1.2.2

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const version = "1.2.1";
+const version = "1.2.2";
 const cacheName = `vfrmanual-${version}`;
 
 const CACHE_URLS = [
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 app=vfrmanualapi
-ver=1.2.1
+ver=1.2.2
 tar="${app}_${ver}.tar"
 gz="${tar}.gz"
 if test -f "$gz"; then

4aadbb67ccbf60b75313e73a243147f45fdbc22b
fa18swiss <fa18swiss@users.noreply.github.com>
2024-01-17 17:47:26 +0100

    Stop changing version in Publish.ps1 and better version loading

--- Publish.ps1
+++ Publish.ps1
@@ -3,24 +3,8 @@ gulp clean
 gulp build
 
 $app = "vfrmanualapi"
-$version = Get-content .\package.json `
-    | Select-String """version"": ""([0-9\.]+)""" `
-    | ForEach-Object { $_.Matches } `
-    | ForEach-Object { $_.Groups[1].Value }
-
+$version = (Get-Content -Raw -Path  .\package.json | ConvertFrom-Json).Version
 Write-Host "Version : $version"
-
-$root = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($MyInvocation.MyCommand.Path, ".."))
-
-$start = [System.IO.Path]::Combine($root, "start.sh")
-$txt = [System.IO.File]::ReadAllText($start).Replace("`r`n", "`n")
-$txt = [System.Text.RegularExpressions.Regex]::Replace($txt, "ver=([0-9\.]+)", "ver=$version")
-[System.IO.File]::WriteAllText($start, $txt)
-
-$sw = [System.IO.Path]::Combine($root, "app", "sw.js")
-$txt = [System.IO.File]::ReadAllText($sw)
-$txt = [System.Text.RegularExpressions.Regex]::Replace($txt, "version = ""([0-9\.]+)""", "version = ""$version""")
-[System.IO.File]::WriteAllText($sw, $txt)
 
 $id = "${app}:${version}"
 $tar = "${app}_${version}.tar"
@@ -41,5 +25,3 @@ Remove-Item $gz -ErrorAction Ignore
 & "${Env:ProgramFiles}\7-zip\7z.exe" a -tgzip $gz $tar
 Remove-Item $tar -ErrorAction Ignore
 Write-Host "Compressed..."
-
-#docker run -d --name=vfrmanualapi -p 127.0.0.1:8003:80 --restart=always -v $pwd\data:/app/data -v $pwd\logs:/logs/ vfrmanualapi
